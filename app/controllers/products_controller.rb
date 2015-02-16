ab894354e00dfa16a94642a12e34b8915066e191
Jennifer Krohn <jenniferlynn1207@msn.com>
2015-02-16 07:46:02 -0500

    with paperclip

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -71,6 +71,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:seller_id, :title, :category, :description, :available_date, :price, :quantity)
+      params.require(:product).permit(:seller_id, :title, :category, :description, :available_date, :price, :quantity, :avatar)
     end
 end
